99e3750d4842d382914e3a57cdf3e83aba4f0b30
George Tsiftsis <gtsiff@gmail.com>
2022-01-09 00:44:01 +0200

    Reduce the code from app.js

---
+++ src/components/HomeScreenDetails.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function HomeScreenDetails() {
+  return (
+    <div className="flex flex-col text-white items-center justify-center">
+      <img src={"./cinema.jpg"} alt="cinema" />
+      <h1 className="max-w-2xl py-12 h-80">
+        A page in which a user could search for a movie and learn details about
+        it. Built it with React and TailwindCSS. Utilise
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-blue-400 underline ml-1 "
+          href="https://www.themoviedb.org/"
+        >
+          The Movie Database API
+        </a>
+      </h1>
+    </div>
+  );
+}
+
+export default HomeScreenDetails;
