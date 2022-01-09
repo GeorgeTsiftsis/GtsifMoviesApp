1884f9479f38a746af31b8a743af3170ec6095a5
George Tsiftsis <gtsiff@gmail.com>
2022-01-09 02:22:21 +0200

    Some  motion moves

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Movie = (props) => {
   const { original_title, overview, id, image } = props;
@@ -9,12 +10,32 @@ const Movie = (props) => {
         ""
       ) : (
         <div className="p-4 flex flex-col   md:flex-row max-w-4xl" key={id}>
-          <img
+          <motion.img
+            initial="hidden"
+            animate="visible"
+            variants={{
+              hidden: {
+                scale: 0.2,
+                opacity: 0.5,
+              },
+              visible: {
+                scale: 1,
+                opacity: 1,
+                transition: {
+                  delay: 0.7,
+                  duration: 2,
+                },
+              },
+            }}
             className="rounded-2xl w-full py-2  md:w-2/4 h-full md:pr-4"
             alt="imageofmovie"
             src={`https://themoviedb.org/t/p/w300/${image}`}
           />
-          <div>
+          <motion.div
+            initial={{ x: -100 }}
+            animate={{ x: 0 }}
+            transition={{ duration: 0.7 }}
+          >
             <h1 className="text-white text-2xl ">{original_title}</h1>
             <p className="text-white justify-text line-clamp-4 ">{overview}</p>
             <button
@@ -23,7 +44,7 @@ const Movie = (props) => {
             >
               Read More
             </button>
-          </div>
+          </motion.div>
           );
         </div>
       )}
--- src/components/MovieInfo.js
+++ src/components/MovieInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Loading from "./Loading";
+import { motion } from "framer-motion";
 
 function MovieInfo(props) {
   const { poster_path, original_title, release_date, overview, vote_average } =
@@ -22,7 +23,14 @@ function MovieInfo(props) {
       </div>
       <div className=" flex flex-col items-center justify-center">
         <h1 className=" text-2xl ">{original_title}</h1>
-        <h1 className=" text-xl ">Date Realeased {release_date}</h1>
+        <motion.h1
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 0.9 }}
+          transition={({ delay: 0.7 }, { yoyo: 800 })}
+          className=" text-xl text-red-700 "
+        >
+          Date Realeased {release_date}
+        </motion.h1>
         <p className="justify-text max-w-2xl p-4  ">{overview}</p>
         <h1 className="py-4">Vote {vote_average} out of 10 </h1>
         <button
